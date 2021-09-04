b14e3cabf13dc05374eaebd7cb6b64ae2c0ce906
peresvetjke <igor.udalov@gmail.com>
2021-09-04 14:27:09 +0300

    Add users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_04_112525) do
+ActiveRecord::Schema.define(version: 2021_09_04_112651) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -43,6 +43,15 @@ ActiveRecord::Schema.define(version: 2021_09_04_112525) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.text "login"
+    t.text "email"
+    t.text "name"
+    t.text "surname"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "answers", "questions"
