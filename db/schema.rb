ad234f84b8c74cce178204255ca5b8acaf76ee16
peresvetjke <igor.udalov@gmail.com>
2021-09-04 14:25:50 +0300

    Add answers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_04_112434) do
+ActiveRecord::Schema.define(version: 2021_09_04_112525) do
+
+  create_table "answers", force: :cascade do |t|
+    t.text "body"
+    t.boolean "correct"
+    t.integer "question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.text "title"
@@ -36,6 +45,7 @@ ActiveRecord::Schema.define(version: 2021_09_04_112434) do
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
+  add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
 end
