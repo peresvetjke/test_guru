cc787ecb2cd94d7e299ee6a45ae676de7cffa266
peresvetjke <igor.udalov@gmail.com>
2021-09-04 14:24:05 +0300

    Add tests table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_04_111911) do
+ActiveRecord::Schema.define(version: 2021_09_04_112224) do
 
   create_table "categories", force: :cascade do |t|
     t.text "title"
@@ -18,4 +18,15 @@ ActiveRecord::Schema.define(version: 2021_09_04_111911) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "tests", force: :cascade do |t|
+    t.text "title"
+    t.integer "level"
+    t.text "reference"
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_tests_on_category_id"
+  end
+
+  add_foreign_key "tests", "categories"
 end
