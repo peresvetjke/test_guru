8c771cbc76d26978cbe6207528e0bf0a66dc2547
peresvetjke <igor.udalov@gmail.com>
2021-09-05 21:45:17 +0300

    Add same_category_tests method to Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,4 +2,8 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :assessments
   has_many :users, through: :assessments
+
+  def self.same_category_tests(category_title)
+    Category.joins(:tests).select('tests.title').distinct.where("categories.title = ?", category_title).order('tests.title DESC').pluck('tests.title')
+  end
 end
