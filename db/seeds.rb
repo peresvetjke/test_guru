1fa8fe78d3be95be4157fb833b5d191e1314829d
peresvetjke <igor.udalov@gmail.com>
2021-09-05 21:43:07 +0300

    Add initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+2.times do |i|
+  Category.create(title: "Category ##{i}")
+end
+
+5.times do |i|
+  Test.create(title: "Test ##{i} (category_id: 1, level: 1)", category_id: 1, level: 1) 
+end
+
+5.times do |i|
+  Test.create(title: "Test ##{i} (category_id: 1, level: 2)", category_id: 1, level: 2) 
+end
+
+5.times do |i|
+  Test.create(title: "Test ##{i} (category_id: 2, level: 1)", category_id: 2, level: 1) 
+end
+
+5.times do |i|
+  Test.create(title: "Test ##{i} (category_id: 2, level: 2)", category_id: 2, level: 2) 
+end
+
+2.times do |i|
+  User.create(login: "User ##{i}")
+end
+
+5.times do |i|
+  Assessment.create(test_id: i, user_id: 1)
+  Assessment.create(test_id: i+5, user_id: 2)
+end
