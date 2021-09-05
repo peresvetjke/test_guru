dd139fd165d93aaf43b19b432f1ba6c46de124d5
peresvetjke <igor.udalov@gmail.com>
2021-09-05 21:44:38 +0300

    Add same_level_tests method to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_many :assessments
   has_many :tests, through: :assessments
+
+  def same_level_tests(level)
+    id = self.id
+    Test.joins(:users).where("users.id = ? and tests.level = ? and assessments.status in (0, 1)", id, level).pluck('id')
+  end
 end
