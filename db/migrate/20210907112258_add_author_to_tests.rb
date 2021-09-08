cda354c7c61296f9b7d246b8bb7076da6f8c9584
peresvetjke <igor.udalov@gmail.com>
2021-09-08 08:35:31 +0300

    Correct author foreign key

--- db/migrate/20210907112258_add_author_to_tests.rb
+++ db/migrate/20210907112258_add_author_to_tests.rb
@@ -1,6 +1,5 @@
 class AddAuthorToTests < ActiveRecord::Migration[6.1]
   def change
-    add_reference :tests, :author, references: :users, index: true, null: false
-    add_foreign_key :tests, :users, column: :author_id
+    add_reference :tests, :author, null: false, foreign_key: {to_table: :users}
   end
 end
