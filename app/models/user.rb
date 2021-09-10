79ce52806965f63f03718e9ed8cc9661870f47e3
peresvetjke <igor.udalov@gmail.com>
2021-09-10 15:43:05 +0300

    Correct User scope method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   validates :login, :email, presence: true
 
-  scope :test_level, -> (level) { Test.joins(:users).where("tests.level = ?", level) }
+  scope :test_level, -> (level) { Test.joins(:assessments).where("tests.level = ?", level) }
 
   def test_level(level)
     User.test_level(level).where("assessments.user_id = ?", id)
