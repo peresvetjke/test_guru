6984d900e178ba3b8d82641f6528c797f969b013
peresvetjke <igor.udalov@gmail.com>
2021-09-15 16:29:39 +0300

    Correct max_answer_per_question validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,17 +1,15 @@
-class MyValidator < ActiveModel::Validator
-  def validate(record)
-    max_amount = 4
-    if record.question.answers.count > max_amount
-      record.errors.add :base, "Превышено кол-во ответов (максимальное значение - #{max_amount})"
-    end
-  end
-end
-
 class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validates_with MyValidator
+  validate :max_answer_per_question, on: :create
 
   scope :correct, -> { where(correct: true) }
+
+  def max_answer_per_question
+    max_amount = 4
+    if self.question.answers.count >= max_amount
+      errors.add :base, "Превышено кол-во ответов (максимальное значение - #{max_amount})"
+    end
+  end
 end
