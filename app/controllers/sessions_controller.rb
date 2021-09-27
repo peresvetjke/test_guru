5239278467583f342a219b619c77dfa468b395ad
peresvetjke <igor.udalov@gmail.com>
2021-09-27 14:58:47 +0300

    Add corrections

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,18 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:email])
+    
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      flash.notice = "Вы успешно авторизованы! Время приступить к тестам!"
-      #redirect_to cookies[:request_path].present? cookies[:request_path] : @user
+      username = @user.login.present? ? @user.login : @user.email
+      flash.notice = "Добро пожаловать, #{username} ! Время приступить к тестам!"
+
       if cookies[:request_path].present?
         redirect_to cookies[:request_path] 
       else
         redirect_to @user
       end
+
     else
       redirect_to login_path, notice: "Не корректные данные авторизации!"
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,6 @@
 class UsersController < ApplicationController
   before_action :user_params, only: :create
 
-  #attr_reader :password
-  #attr_writer :password_confirmation
-
   def new
     @user = User.new
   end
