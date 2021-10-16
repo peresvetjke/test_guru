db3c0b19a437c6960fb942e30b3a90c0d79fa8c9
peresvetjke <igor.udalov@gmail.com>
2021-10-16 13:38:18 +0300

    Add corrections

--- app/controllers/contact_messages_controller.rb
+++ app/controllers/contact_messages_controller.rb
@@ -1,14 +1,13 @@
 class ContactMessagesController < ApplicationController
+  
   def new
+
   end
 
   def create
-    byebug
     @contact_message = ContactMessage.new(contact_message_params)
-    byebug
     if @contact_message.valid?
       UserMailer.with(contact_message: @contact_message).contact_email.deliver_now
-      # UserMailer.with(email: @contact_message.email, subject: @contact_message.subject, content: @contact_message.content).contact_email.deliver_now
       redirect_to root_path, notice: "Message sent! Thank you for contacting us."
     else
       render :new
