7d5f9ca4c772ac1ffe5042c8c2b0bf2a85ba2af6
peresvetjke <igor.udalov@gmail.com>
2021-10-16 08:32:54 +0300

    Config to psql and add indexes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_05_060147) do
+ActiveRecord::Schema.define(version: 2021_10_16_050600) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -69,6 +72,7 @@ ActiveRecord::Schema.define(version: 2021_10_05_060147) do
     t.integer "author_id", null: false
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
 
   create_table "users", force: :cascade do |t|
