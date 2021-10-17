96df313a9b8b29b4430f6593cea62e3565a72b05
peresvetjke <igor.udalov@gmail.com>
2021-10-17 18:51:07 +0300

    Add corrections

--- app/controllers/contact_messages_controller.rb
+++ app/controllers/contact_messages_controller.rb
@@ -5,7 +5,6 @@ class ContactMessagesController < ApplicationController
   end
 
   def create
-    byebug
     @contact_message = current_user.contact_messages.new(contact_message_params)
     if @contact_message.save
       UserMailer.with(contact_message: @contact_message).contact_email.deliver_now
