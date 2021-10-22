57eb774dd60cf9fdc74482d52982de5ee9b54ff5
peresvetjke <igor.udalov@gmail.com>
2021-10-22 10:38:48 +0300

    Add corrections

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -3,23 +3,4 @@ class Rule < ApplicationRecord
   has_many :badges
 
   validates :title, :method, presence: true
-  # validate :maintained?
-
-  def relevant_tests
-    query = Test
-    query = query.where(category_id: self.category.id) if self.category.present?
-    query = query.where(level: self.level) if self.level.present? 
-    query
-  end
-
-  private
-
-  # для правила хватит имя правила для отображения(если хочешь), имя правила для вызова метода проверки, значение параметра для правила
-  # а если будут еще правила какие-то ты будешь новые поля добавлять? и в итоге у тебя будет мусор в таблице, 
-  # где почти все строки пустые, а только парочка имеет значение. надо придумать другой подход
-  #
-
-  # def maintained?
-  #   errors.add :base, "Правило не заполнено." if self.category_id.nil? && self.level.nil? unless self.first_try.present?
-  # end
 end
