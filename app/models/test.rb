21fa34fa929a67331bf1e8d1a8f344aa382f2a7e
peresvetjke <igor.udalov@gmail.com>
2021-10-26 16:37:35 +0300

    Add corrections

--- app/models/test.rb
+++ app/models/test.rb
@@ -25,15 +25,7 @@ class Test < ApplicationRecord
   def finalized?
     self.has_min_questions_amount? && !self.has_questions_without_answers? && !self.has_questions_without_correct_answers?
   end
-
-  # Badges
-  def self.test_ids_passed_by_category(category)
-
-  end
-
-  def self.ids_by_level(category)
-  end
-
+  
   private
 
   def has_min_questions_amount?
